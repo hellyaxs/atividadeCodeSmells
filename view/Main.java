310544580d6efd6987158ad2b0757be4cef5029e
Elias <eliasvitor.dev@gmail.com>
2024-02-15 09:09:53 -0300

    removendo codigo duplicado

--- view/Main.java
+++ view/Main.java
@@ -12,22 +12,26 @@ public class Main {
         ArrayList<String> colors = new ArrayList<>(Arrays.asList("1-vermelho", "2-azul", "3-verde") );
         Game game = Game.getInstance(colors);
 
-        System.out.println("Bem-vindo ao WAR!");
+        showMessage("Bem-vindo ao WAR!");
 
-        System.out.println("Cores disponíveis:");
+        showMessage("Cores disponíveis:");
         ArrayList<String> coresDisponiveis = game.getAvailableColors();
 
-        System.out.print("Escolha sua cor (Digite o número correspondente): ");
+        showMessage("Escolha sua cor (Digite o número correspondente): ");
         int escolha = scanner.nextInt();
 
         if (escolha >= 1 && escolha <= coresDisponiveis.size()) {
             String corEscolhida = coresDisponiveis.get(escolha -1);
-            System.out.println("Você escolheu a cor " + corEscolhida + ". Boa sorte no jogo!");
+            showMessage("Você escolheu a cor " + corEscolhida + ". Boa sorte no jogo!");
         } else {
-            System.out.println("Escolha inválida. Por favor, escolha um número válido.");
+            showMessage("Escolha inválida. Por favor, escolha um número válido.");
         }
 
         // Fechando o scanner
         scanner.close();
+    }
+
+    static void showMessage(String msg){
+        System.out.println(msg);
     }
 }
